5d5e04d10e81c0aa7bf355c7ef96fd388fb5c8bb
Wladimir J. van der Laan <laanwj@gmail.com>
2012-12-01 17:07:47 +0100

    add license headers

--- python/disas-util.h
+++ python/disas-util.h
@@ -1,3 +1,28 @@
+/*
+ * Target-independent utilities for disassembling instructions to text format 
+ * using qemu's built-in libbfd. 
+ * These are basically wrappers to print to a memory buffer instead of a stream.
+ *
+ * Copyright (c) 2012 Wladimir J. van der Laan
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 #ifndef H_DISAS_UTIL
 #define H_DISAS_UTIL
 #include "dis-asm.h"
